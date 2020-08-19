9e56283ae7dac1b56d167171f61c187d2b5c6efe
Judicael <djmx972@gmail.com>
2020-08-19 21:01:36 +0200

    add keyspace in config

--- src/config/db/cassandra.js
+++ src/config/db/cassandra.js
@@ -3,6 +3,7 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({
     contactPoints: ['127.0.0.1'],
     localDataCenter: 'datacenter1',
+    keyspace: 'shoutapp',
     credentials: { username: 'cassandra', password: 'cassandra' }
 });
 client.connect(function (err, result) {
--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,28 +1,22 @@
 
 const UserService = require('../service/UserService');
-const {client}=require('../config/db/cassandra');
+const { client } = require('../config/db/cassandra');
 
 //const userService = new UserService();
 
 exports.getUsers = async (req, res, next) => {
 
- const getAllUsers = 'SELECT * FROM shoutapp.users' ;
 
-    /*const result =userService.findUser(id).catch(function (error) {
-        console.error('Error saving document', error);
-        return res.status(500).json(error);
-    });*/
+    // the key space "shoutapp" is already in cassandra config file
+    const getAllUsers = 'SELECT * FROM users';
 
-   /* client.execute(getAllUsers,[], function(err,result){
-        if(err){
-            return res.status(404).json({"error":"oups"});
-        }
-    });*/
+    //const getAllUsers = 'SELECT * FROM shoutapp.users' ;
 
-    const result= await client.execute(getAllUsers,[] )
-    .catch((error)=> {
-        console.log(error);
-        res.status(404).json({"error":"oups"})});
+    const result = await client.execute(getAllUsers, [])
+        .catch((error) => {
+            console.log(error);
+            res.status(404).json({ "error": "oups" })
+        });
     console.log(result.rows);
 
     return res.status(200).json(result.rows);
